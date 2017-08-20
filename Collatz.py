57a0036e4cc055fee3d8bc77aae01624942c087c
Tvales <timv96@vt.edu>
2017-08-20 18:49:12 -0400

    trying out new commit

---
+++ Collatz.py
@@ -0,0 +1,16 @@
+#collatz program. function that eventually returns a 1 given any integer given certain rules
+
+def collatz(numCollatz):
+    if numCollatz % 2 == 0:
+        numCollatz = numCollatz // 2
+    else:
+        numCollatz = 3 * numCollatz + 1
+    return numCollatz
+
+num = 3223;
+
+while num != 1:
+    num = collatz(num)
+    print(num)
+
+print("Tim is Amazing")
