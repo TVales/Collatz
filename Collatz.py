19fa571311c9ab68810b516feca8f4a7ce1f3557
Tvales <timv96@vt.edu>
2017-10-06 12:04:37 -0400

    Comments added

--- Collatz.py
+++ Collatz.py
@@ -1,5 +1,7 @@
 #collatz program. function that eventually returns a 1 given any integer given certain rules
 
+#if num is even divide by 2. // is integer division. / is floating point
+#otherwise multiply by 3 and add 1
 def collatz(numCollatz):
     if numCollatz % 2 == 0:
         numCollatz = numCollatz // 2
@@ -7,8 +9,10 @@ def collatz(numCollatz):
         numCollatz = 3 * numCollatz + 1
     return numCollatz;
 
+#type cast the input to an integer so that it will work
 num = int(input("Enter a number  "));
 
+#while num given doesnt equal 1 keep calling collatz function
 while num != 1:
     num = collatz(num)
     print(num)
